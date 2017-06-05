22fb61047a42a873d8ab04927afc8cb84cd27b52
BJCheng <BJCheng@users.noreply.github.com>
2017-06-05 13:44:02 +0800

    lab2 advanced css and flex-box

---
+++ lab2/app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname+'/public/lab2.html');
+})
+
+app.get('*', (req, res)=>{
+    res.status(404).send('page not found');
+});
+
+app.set('port', 3000);
+app.listen(app.get('port'), ()=>{
+    console.log(`server listening at port ${app.get('port')}`);
+})
