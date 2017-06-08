603471dd94342d1156853d8cfdab1db146eb759e
BJCheng <BJCheng@users.noreply.github.com>
2017-06-08 21:49:39 +0800

    transpiled with babel

--- lab2/transpiled-app.js
+++ lab2/transpiled-app.js
@@ -4,15 +4,15 @@ var app = express();
 
 app.use(express.static('public'));
 
-app.get('/', (req, res)=>{
-    res.sendFile(__dirname+'/public/lab2.html');
-})
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/lab2.html');
+});
 
-app.get('*', (req, res)=>{
+app.get('*', (req, res) => {
     res.status(404).send('page not found');
 });
 
 app.set('port', 3000);
-app.listen(app.get('port'), ()=>{
+app.listen(app.get('port'), () => {
     console.log(`server listening at port ${app.get('port')}`);
-})
+});
