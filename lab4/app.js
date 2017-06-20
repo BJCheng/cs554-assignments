7524b708e89bc3cdabc517e912fa01ad0126b097
BJCheng <BJCheng@users.noreply.github.com>
2017-06-20 17:21:07 +0800

    add heroku configs

--- lab4/app.js
+++ lab4/app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 
 configRoutes(app);
 
-app.set('port', 3000);
+app.set('port', process.env.PORT ||3000);
 app.listen(app.get('port'), () => {
     console.log(`server listening at ${app.get('port')}`);
 });
